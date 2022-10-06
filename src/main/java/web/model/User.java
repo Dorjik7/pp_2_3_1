7b9e7728a296ce3693f706fa4491f18f468d87b4
Maria Dryukova <mdryukova@yandex.ru>
2022-10-06 16:37:07 +0300

    initial commit

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,92 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column(name = "age")
+    private int age;
+    @Column(name = "email")
+
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String second_name, int age, String email) {
+
+        this.firstName = name;
+        this.secondName = second_name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String second_name) {
+        this.secondName = second_name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+
+        return "User{" +
+                "id=" + id +
+                ", name='" + firstName + '\'' +
+                ", second_name='" + secondName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
+
+
+
+
